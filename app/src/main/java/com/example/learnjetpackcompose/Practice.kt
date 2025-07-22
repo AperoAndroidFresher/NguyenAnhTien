4c6ea4f4a451b90fdad2338e3160d298ddae1ff5
TienNA <tienna@talent.apero.vn>
2025-07-22 17:48:36 +0700

    Lesson6

---
+++ app/src/main/java/com/example/learnjetpackcompose/Practice.kt
@@ -0,0 +1,11 @@
+package com.example.learnjetpackcompose
+
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
