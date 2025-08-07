2d33bd388872fd969fc9eda03f09ef5ea1b6417e
TienNA <tienna@talent.apero.vn>
2025-08-07 14:51:35 +0700

    add feature save internal storage

--- app/src/main/java/com/example/learnjetpackcompose/di/NetworkModule.kt
+++ app/src/main/java/com/example/learnjetpackcompose/di/NetworkModule.kt
@@ -1,9 +1,12 @@
 package com.example.learnjetpackcompose.di
 
+import android.content.Context
+import com.example.learnjetpackcompose.Utils.Mp3Downloader
 import com.example.learnjetpackcompose.data.api.ApiService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -26,5 +29,13 @@ object NetworkModule {
     @Singleton
     fun provideSongApiService(retrofit: Retrofit): ApiService {
         return retrofit.create(ApiService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideMp3Downloader(
+        @ApplicationContext context: Context
+    ): Mp3Downloader {
+        return Mp3Downloader(context)
     }
 }
